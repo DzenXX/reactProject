62af4d2aee5e7130f87019974b10ff2dfeaed58c
dzenx <you@example.com>
2022-04-15 00:36:28 +0300

    after 54th lesson

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -15,6 +15,7 @@ let User = (props) => {
             <div className={s.content}>
                 <img className={s.logo} src={ props.photos.small != null ? props.photos.small : "https://static.turbosquid.com/Preview/2015/05/19__15_58_37/SULLAFinal.jpg13871ffb-9e86-49b9-9211-4fa721216a9bLarge.jpg"} alt="#"/>
                 <div className={s.content__info}>
+                    <div className={s.content__name}>{props.name}</div>
                     <div className={s.content__status}>
                         { props.status != null ? props.status : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at consequuntur, dicta eius, fugiat, optio possimus ratione reprehenderit rerum suscipit temporibus voluptates. Amet autem fugiat iusto maxime quam voluptate voluptates.'}
                     </div>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,42 +1,39 @@
 import User from "./User/User";
+import React from "react";
 import s from './Users.module.css'
 import * as axios from "axios";
 
-let Users = (props) => {
-    // let newUsers = [
-    //     {name: 'Anya', id: 'ann', followed: true, status: 'Everything is fine', location: 'Bobruisk, Belarus', avatar: 'https://avatars.mds.yandex.net/get-zen_doc/3415135/pub_61616cb51acf7f5d7fbf3c44_616173587bb9ec38f6a25f73/scale_1200'},
-    //     {name: 'Egor', id: 'egor', followed: true, status: 'Everything is fine', location: 'Minsk, Belarus', avatar: 'https://avatars.mds.yandex.net/get-zen_doc/3415135/pub_61616cb51acf7f5d7fbf3c44_616173587bb9ec38f6a25f73/scale_1200'},
-    //     {name: 'Sasha', id: 'alexander', followed: false, status: 'Everything is fine', location: 'Minsk, Belarus', avatar: 'https://avatars.mds.yandex.net/get-zen_doc/3415135/pub_61616cb51acf7f5d7fbf3c44_616173587bb9ec38f6a25f73/scale_1200'},
-    //     {name: 'Vlad', id: 'vladislav', followed: true, status: 'Everything is fine', location: 'Harkov, Ukraine', avatar: 'https://avatars.mds.yandex.net/get-zen_doc/3415135/pub_61616cb51acf7f5d7fbf3c44_616173587bb9ec38f6a25f73/scale_1200'},
-    // ]
- let getUsers = (e) => {
-     e.target.style.display = 'none'
-     if (props.users.length === 0) {
-         // props.setUsers(newUsers)
-         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-             debugger;
-             props.setUsers(response.data.items)
-         })
-     }
-    }
-
-    debugger;
-    let showUsers = () => {
-        debugger;
+class Users extends React.Component {
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
+            this.props.setUsers(response.data.items)
         })
     }
+
+
+showUsers = () => {
     debugger;
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+        this.props.setUsers(response.data.items)
+    })
+}
+debugger;
+
+render()
+{
     return (
         <div>
-            <div className={s.button}><button onClick={getUsers}>Get users</button></div>
+
             {
-                props.users.map(u => (<User photos={u.photos} follow={props.follow} unfollow={props.unfollow} followed={u.followed} userId={u.id} status={u.status} />))
+                this.props.users.map(u => (
+                    <User photos={u.photos} follow={this.props.follow} unfollow={this.props.unfollow}
+                          followed={u.followed} name={u.name}
+                          userId={u.id} status={u.status}/>))
             }
-            <button className={s.button} onClick={showUsers}>Show more</button>
+            <button className={s.button} onClick={this.showUsers}>Show more</button>
         </div>
     )
+}
 }
 
 export default Users
